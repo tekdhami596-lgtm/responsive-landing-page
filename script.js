88d598617b68a894ef499af1aaf7a5de29e4017c
tekdhami596--lgtm <tekdhami596@gmail.com>
2025-11-27 08:29:37 +0545

    add responsive landing page

---
+++ script.js
@@ -0,0 +1,8 @@
+ document.getElementById('menu').addEventListener('click', () => {
+            document.getElementById("sidebar").classList.toggle("active")
+        })
+
+        // document.getElementById('close-icon').addEventListener('click', () => {
+        //     document.querySelector("#sidebar").classList.toggle("active")
+
+        // })
